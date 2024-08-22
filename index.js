6b215cb0ee5afd00fd0c6b38234ecf5bf9fed564
Namit298 <Biztoindia5@gmail.com>
2024-08-22 15:17:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const port = 3000;
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
-app.use(cors());
+ app.use(cors({
+    origin: 'https://fairfoxeonnoida.online',
+     
+     
+}));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
